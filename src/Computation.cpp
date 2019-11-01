dea24961109f1cdfb6498c90db3ade77c286574b
Fabian Jobst <fabi-jo@web.de>
2019-11-01 14:35:52 +0100

    Computation RB

--- src/Computation.cpp
+++ src/Computation.cpp
@@ -58,20 +58,65 @@ void Computation::computeTimeStepWidth() {
 }
 
 void Computation::applyBoundaryValues() {
+    //todo: Zellen in den Ecken werden noch ignoriert, evtl. nach Bedarf ergänzen
 
+    // u
+    //unterer Rand
+    int j = discretization_.get()->uJBegin();
+    for (int i = discretization_.get()->uIBegin() + 1; i <= discretization_.get()->uIEnd() - 1; i++) {
+        discretization_.get()->u(i, j) = 2 * settings_.dirichletBcBottom[0] - discretization_.get()->u(i, j + 1);
+    }
+    //rechter und linker Rand
+    int i_low = discretization_.get()->uIBegin();
+    int i_high = discretization_.get()->uIEnd();
+    for (j = discretization_.get()->uJBegin() + 1; j <= discretization_.get()->uJEnd() - 1; j++) {
+        discretization_.get()->u(i_low, j) = settings_.dirichletBcLeft[0];
+        discretization_.get()->u(i_high, j) = settings_.dirichletBcRight[0];
+    }
+    // oberer Rand
+    j = discretization_.get()->uJEnd();
+    for (int i = discretization_.get()->uIBegin() + 1; i <= discretization_.get()->uIEnd() - 1; i++) {
+        discretization_.get()->u(i, j) = 2 * settings_.dirichletBcTop[0] - discretization_.get()->u(i, j - 1);
+    }
+
+    // v
+    //unterer Rand
+    int j = discretization_.get()->vJBegin();
+    for (int i = discretization_.get()->vIBegin() + 1; i <= discretization_.get()->vIEnd() - 1; i++) {
+        discretization_.get()->v(i, j) = settings_.dirichletBcBottom[1] - discretization_.get()->v(i, j + 1);
+    }
+    //rechter und linker Rand
+    int i_low = discretization_.get()->vIBegin();
+    int i_high = discretization_.get()->vIEnd();
+    for (j = discretization_.get()->vJBegin() + 1; j <= discretization_.get()->vJEnd() - 1; j++) {
+        discretization_.get()->v(i_low, j) = 2 * settings_.dirichletBcLeft[1] - discretization_.get()->v(i_low + 1, j);
+        discretization_.get()->v(i_high, j) =
+                2 * settings_.dirichletBcRight[1] - discretization_.get()->v(i_high - 1, j);
+    }
+    // oberer Rand
+    j = discretization_.get()->vJEnd();
+    for (int i = discretization_.get()->vIBegin() + 1; i <= discretization_.get()->vIEnd() - 1; i++) {
+        discretization_.get()->v(i, j) = 2 * settings_.dirichletBcTop[1] - discretization_.get()->v(i, j - 1);
+    }
 
 }
 
 void Computation::PreliminaryVelocities() {
     for (int j = grid.uJBegin; j <= grid.uJEnd; j++) {
         for (int i = grid.uIBegin; i <= grid.uIEnd; i++) {
-            grid.f(i, j) = grid.u(i,j) + dt_ * (1/Re_ * (discretization_.computeD2uDx2(i,j) + discretization_.computeD2uDy2(i,j)) - discretization_.computeDu2Dx(i,j) - discretization_.computeDuvDy(i,j) + settings_.g[0]);
+            grid.f(i, j) = grid.u(i, j) + dt_ * (1 / Re_ * (discretization_.computeD2uDx2(i, j) +
+                                                            discretization_.computeD2uDy2(i, j)) -
+                                                 discretization_.computeDu2Dx(i, j) -
+                                                 discretization_.computeDuvDy(i, j) + settings_.g[0]);
         }
     }
 
     for (int j = grid.vJBegin; j <= grid.vJEnd; j++) {
         for (int i = grid.vIBegin; i <= grid.vIEnd; i++) {
-            grid.g(i, j) = grid.v(i,j) + dt_ * (1/Re_ * (discretization_.computeD2vDy2(i,j) + discretization_.computeD2vDx2(i,j)) - discretization_.computeDv2Dy(i,j) - discretization_.computeDuvDx(i,j) + settings_.g[1]);
+            grid.g(i, j) = grid.v(i, j) + dt_ * (1 / Re_ * (discretization_.computeD2vDy2(i, j) +
+                                                            discretization_.computeD2vDx2(i, j)) -
+                                                 discretization_.computeDv2Dy(i, j) -
+                                                 discretization_.computeDuvDx(i, j) + settings_.g[1]);
         }
     }
 }
