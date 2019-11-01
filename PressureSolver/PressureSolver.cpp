0af8aa7ff1aa9b13c844c97279efb65586730eab
Fabian Jobst <fabi-jo@web.de>
2019-11-01 13:05:47 +0100

    SOR fertig

--- PressureSolver/PressureSolver.cpp
+++ PressureSolver/PressureSolver.cpp
@@ -14,6 +14,22 @@ PressureSolver::PressureSolver(std::shared_ptr<Discretization> discretization, d
 }
 
 void PressureSolver::setBoundaryValues() {
-
-
+    //todo: Zellen in den Ecken werden noch ignoriert, evtl. nach Bedarf ergänzen
+    //unterer Rand
+    int j = discretization_.get()->pJBegin();
+    for (int i = discretization_.get()->pIBegin() + 1; i <= discretization_.get()->pIEnd() - 1; i++) {
+        discretization_.get()->p(i, j) = discretization_.get()->p(i, j + 1);
+    }
+    //rechter und linker Rand
+    int i_low = discretization_.get()->pIBegin();
+    int i_high = discretization_.get()->pIEnd();
+    for (j = discretization_.get()->pJBegin() + 1; j <= discretization_.get()->pJEnd() - 1; j++) {
+        discretization_.get()->p(i_low, j) = discretization_.get()->p(i_low + 1, j);
+        discretization_.get()->p(i_high, j) = discretization_.get()->p(i_high - 1, j);
+    }
+    // oberer Rand
+    j = discretization_.get()->pJEnd();
+    for (int i = discretization_.get()->pIBegin() + 1; i <= discretization_.get()->pIEnd() - 1; i++) {
+        discretization_.get()->p(i, j) = discretization_.get()->p(i, j - 1);
+    }
 }
--- PressureSolver/SOR.cpp
+++ PressureSolver/SOR.cpp
@@ -3,21 +3,54 @@
 //
 
 #include "SOR.h"
+#include <cmath>
 
-SOR::SOR(std::shared_ptr<Discretization> discretization, double epsilon, int maximumNumberOfIterations, double omega):
-    PressureSolver(discretization,epsilon,maximumNumberOfIterations),
-    omega(omega){
+SOR::SOR(std::shared_ptr<Discretization> discretization, double epsilon, int maximumNumberOfIterations, double omega) :
+        PressureSolver(discretization, epsilon, maximumNumberOfIterations),
+        omega(omega) {
 
 }
 
 void SOR::solve() {
-    int a = 2;
-    while(a <2){
-        a++;
-        for(int j =0; j<1; j++){
-            for(int i=0; i<1; i++){
-                discretization_.get()->p(i,j) = discretization_.get()->p(i,j) + ;
+    int iter = 1;
+    double eps = 1;
+    while (iter <= maximumNumberOfIterations_ && eps > epsilon_) {
+        iter++;
+        setBoundaryValues();
+        for (int j = discretization_.get()->pJBegin() + 1; j <= discretization_.get()->pJEnd() - 1; j++) {
+            for (int i = discretization_.get()->pIBegin() + 1; i <= discretization_.get()->pIEnd() - 1; i++) {
+                discretization_.get()->p(i, j) = (1 - omega) * discretization_.get()->p(i, j) +
+                                                 omega *
+                                                 pow(discretization_.get()->dx(), 2) *
+                                                 pow(discretization_.get()->dy(), 2) /
+                                                 2 * (pow(discretization_.get()->dx(), 2) +
+                                                      pow(discretization_.get()->dy(), 2)) *
+                                                 ((discretization_.get()->p(i - 1, j) +
+                                                   discretization_.get()->p(i + 1, j)) /
+                                                  pow(discretization_.get()->dx(), 1) +
+                                                  (discretization_.get()->p(i, j - 1) +
+                                                   discretization_.get()->p(i, j + 1)) /
+                                                  pow(discretization_.get()->dy(), 2) -
+                                                  discretization_.get()->rhs(i, j));
             }
         }
+        //residuum
+        eps = 0;
+        for (int j = discretization_.get()->pJBegin() + 1; j <= discretization_.get()->pJEnd() - 1; j++) {
+            for (int i = discretization_.get()->pIBegin() + 1; i <= discretization_.get()->pIEnd() - 1; i++) {
+                eps = eps + pow((discretization_.get()->p(i - 1, j) -
+                                 2 * discretization_.get()->p(i, j) +
+                                 discretization_.get()->p(i + 1, j)) /
+                                pow(discretization_.get()->dx(), 2) +
+                                (discretization_.get()->p(i, j - 1) -
+                                 2 * discretization_.get()->p(i, j) +
+                                 discretization_.get()->p(i, j + 1)) /
+                                pow(discretization_.get()->dy(), 2) -
+                                discretization_.get()->rhs(i, j),
+                                2);
+            }
+        }
+        eps = sqrt(eps);
     }
+    setBoundaryValues();
 }
