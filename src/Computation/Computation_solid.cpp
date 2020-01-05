726d9c1acbb4fcbde1326c01d7d5c6330ae605a0
JuliaPelzer <jupelzer@outlook.de>
2020-01-05 10:34:48 +0100

    Computation_solid changed

--- src/Computation/Computation_solid.cpp
+++ src/Computation/Computation_solid.cpp
@@ -42,7 +42,7 @@ void Computation_solid::initialize(int argc, char **argv) {
 
     //initialize IMPLICIT temperatureSolver
     GaussSeidel tSolver(discretization_, geometry_, settings_.epsilon, settings_.maximumNumberOfIterations,dt_, settings_.heatDiffusivity);
-    temperatureSolver_ = make_unique<GaussSeidel>(tSolver); //TODO not working but also not needed?
+    temperatureSolver_ = make_unique<GaussSeidel>(tSolver); //TODO not working 
 
     //initialize outputWriters
     OutputWriterText outText(discretization_);
@@ -83,106 +83,6 @@ void Computation_solid::runSimulation() {
 
 }
 
-void Computation_solid::applyBoundaryValuesTemperature() {
-    // help variables
-    int i_low;
-    int i_high;
-    int j_low;
-    int j_high;
-
-    //outer bounds
-    //left bound without corners
-    i_low = discretization_.get()->tIBegin() - 1;
-    for (int j = discretization_.get()->tJBegin() - 1; j <= discretization_.get()->tJEnd() + 1; j++) {
-        if (!geometry_.get()->isFluid(i_low, j)) {
-            if (geometry_.get()->get_temperature(i_low, j).first == "TN") {
-                discretization_.get()->t(i_low, j) = discretization_.get()->t(i_low + 1, j) -
-                                                     discretization_.get()->dx() *
-                                                     geometry_.get()->get_temperature(i_low, j).second[0];
-            } else { //"TD"
-                discretization_.get()->t(i_low, j) = 2 * geometry_.get()->get_temperature(i_low, j).second[0] -
-                                                     discretization_.get()->t(i_low + 1, j);
-            }
-        }
-    }
-
-    //right bound without corners
-    i_high = discretization_.get()->tIEnd() + 1;
-    for (int j = discretization_.get()->tJBegin() - 1; j <= discretization_.get()->tJEnd() + 1; j++) {
-        if (!geometry_.get()->isFluid(i_high, j)) {
-            if (geometry_.get()->get_temperature(i_high, j).first == "TN") {
-                discretization_.get()->t(i_high, j) = discretization_.get()->t(i_high - 1, j) -
-                                                      discretization_.get()->dx() *
-                                                      geometry_.get()->get_temperature(i_high, j).second[0];
-            } else {
-                discretization_.get()->t(i_high, j) = 2 * geometry_.get()->get_temperature(i_high, j).second[0] -
-                                                      discretization_.get()->t(i_high - 1, j);
-            }
-        }
-    }
-
-    //bottom bound with corners
-    j_low = discretization_.get()->tJBegin() - 1;
-    for (int i = discretization_.get()->tIBegin(); i <= discretization_.get()->tIEnd(); i++) {
-        if (!geometry_.get()->isFluid(i, j_low)) {
-            if (geometry_.get()->get_temperature(i, j_low).first == "TN") {
-                discretization_.get()->t(i, j_low) = discretization_.get()->t(i, j_low + 1) -
-                                                     discretization_.get()->dy() *
-                                                     geometry_.get()->get_temperature(i, j_low).second[0];
-            } else {
-                discretization_.get()->t(i, j_low) = 2 * geometry_.get()->get_temperature(i, j_low).second[0] -
-                                                     discretization_.get()->t(i, j_low + 1);
-            }
-        }
-    }
-
-    //top bound with corners
-    j_high = discretization_.get()->tJEnd() + 1;
-    for (int i = discretization_.get()->tIBegin(); i <= discretization_.get()->tIEnd(); i++) {
-        if (!geometry_.get()->isFluid(i, j_high)) {
-            if (geometry_.get()->get_temperature(i, j_high).first == "TN") {
-                discretization_.get()->t(i, j_high) = discretization_.get()->t(i, j_high - 1) -
-                                                      discretization_.get()->dy() *
-                                                      geometry_.get()->get_temperature(i, j_high).second[0];
-            } else {
-                discretization_.get()->t(i, j_high) = 2 * geometry_.get()->get_temperature(i, j_high).second[0] -
-                                                      discretization_.get()->t(i, j_high - 1);
-            }
-        }
-    }
-
-    //inner cells (obstacles)
-    for (int j = discretization_.get()->tJBegin(); j <= discretization_.get()->tJEnd(); j++) {
-        for (int i = discretization_.get()->tIBegin(); i <= discretization_.get()->tIEnd(); i++) {
-            if (!geometry_.get()->isFluid(i, j)) {
-                int N = 0; //number of neighbouring fluid cells
-                double tTmp = 0;
-
-                //check left and right neighbours
-                if (geometry_.get()->isFluid(i - 1, j)) {
-                    tTmp += discretization_.get()->t(i - 1, j);
-                    N++;
-                } else if (geometry_.get()->isFluid(i + 1, j)) {
-                    tTmp += discretization_.get()->t(i + 1, j);
-                    N++;
-                }
-                //check top and bottom neighbours
-                if (geometry_.get()->isFluid(i, j - 1)) {
-                    tTmp += discretization_.get()->t(i, j - 1);
-                    N++;
-                } else if (geometry_.get()->isFluid(i, j + 1)) {
-                    tTmp += discretization_.get()->t(i, j + 1);
-                    N++;
-                }
-                //assign average value
-                if (N > 0) {
-                    discretization_.get()->t(i, j) = tTmp / N;
-                }
-            }
-        }
-    }
-}
-
 void Computation_solid::computeTemperature(Array2D tTmp) {
     for (int j = discretization_.get()->tJBegin(); j <= discretization_.get()->tJEnd(); j++) {
         for (int i = discretization_.get()->tIBegin(); i <= discretization_.get()->tIEnd(); i++) {
@@ -192,7 +92,7 @@ void Computation_solid::computeTemperature(Array2D tTmp) {
         }
     }
     
-    temperatureSolver_->solve();
+    temperatureSolver_->solve(tTmp);
 }
 
 /*
