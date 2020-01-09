73fa183aad24d223e5db947b3cc92f6a0b43f4d9
Scriptkiddi <fritz@otlinghaus.it>
2020-01-09 10:24:55 +0100

    Create output dirs

--- src/output_writer/output_writer.cpp
+++ src/output_writer/output_writer.cpp
@@ -7,7 +7,8 @@ OutputWriter::OutputWriter(shared_ptr<Discretization> discretization, Settings s
  : discretization_(discretization), settings_(settings), fileNo_(0)
 {
   // create "out" subdirectory if it does not yet exist
-  int returnValue = system("mkdir -p out");
+  std::string out = "mkdir -p out_" + settings_.participantName;
+  int returnValue = system(out.c_str());
   if (returnValue != 0)
     std::cout << "Could not create subdirectory \"out\"." << std::endl;
 }
