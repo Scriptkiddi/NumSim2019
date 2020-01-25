0e1b3478637da254764bce1aa750fa6608a898b2
JuliaPelzer <jupelzer@outlook.de>
2020-01-25 16:09:57 +0100

    output writer . cpp

--- src/output_writer/output_writer.cpp
+++ src/output_writer/output_writer.cpp
@@ -2,8 +2,8 @@
 
 #include <iostream>
 
-OutputWriter::OutputWriter(std::shared_ptr<Discretization> discretization)
- : discretization_(discretization), fileNo_(0)
+OutputWriter::OutputWriter(std::shared_ptr<StaggeredGrid> staggeredGrid)
+ : staggeredGrid_(staggeredGrid), fileNo_(0)
 {
   // create "out" subdirectory if it does not yet exist
   int returnValue = system("mkdir -p out");
